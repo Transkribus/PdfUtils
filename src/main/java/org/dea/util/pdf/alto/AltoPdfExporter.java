ee8106b78d2017c7972b4fc5ea5d56979241cbbf
phil <phil@0dd61c38-6e9d-4b9c-aaca-00e9206e903d>
2014-11-21 08:57:35 +0000

    git-svn-id: svn+ssh://dbis-halvar.uibk.ac.at/home/svn/PdfUtils/trunk@9476 0dd61c38-6e9d-4b9c-aaca-00e9206e903d

--- src/main/java/org/dea/util/pdf/alto/AltoPdfExporter.java
+++ src/main/java/org/dea/util/pdf/alto/AltoPdfExporter.java
@@ -7,8 +7,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 import java.util.Observable;
 
 import org.apache.commons.lang3.tuple.Pair;
