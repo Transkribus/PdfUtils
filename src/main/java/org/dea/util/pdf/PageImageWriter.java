53e0a652f2c3e324dba67c2ac1b8e4d269ddfcee
Eva Lang <eva.lang@bistum-passau.de>
2016-06-14 07:14:29 +0200

    Adding PageImageWriter to render complete page as png image

---
+++ src/main/java/org/dea/util/pdf/PageImageWriter.java
@@ -0,0 +1,86 @@
+package org.dea.util.pdf;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+import com.itextpdf.text.DocumentException;
+
+public class PageImageWriter {
+	protected String extractDir;
+	
+	/**
+	 * Object constructor uses user's temporary directory by default
+	 */
+	public PageImageWriter() {
+		this.extractDir = System.getProperty("java.io.tmpdir") + "img" + File.separator;
+	}	
+	
+
+    public String getExtractDirectory() {
+    	return extractDir;
+    }
+    
+    /**
+     * Parses a PDF document and renders all images to an output directory.
+     * @param filepath Path to PDF document
+     * @param outDir Output directory will be expanded with pdf basename
+     * @throws IOException
+     * @throws DocumentException
+     * @return name of created folder
+     */
+    public String extractImages(String filepath, String outDir) 
+    	throws IOException, SecurityException {
+    	
+        
+    	// extract file name
+    	File file = new File(filepath);
+    	final String name = FilenameUtils.getBaseName(file.getName());
+
+    	// create file reader for input pdf
+    	PDDocument document = PDDocument.load(file);
+    	PDFRenderer renderer = new PDFRenderer(document);    	        
+        // create output folder/filename(s)
+        File dir = new File(outDir + File.separator + name);
+        dir.mkdir();
+        
+        // set extract directory name
+        extractDir = dir.getPath();
+        
+        final String out = extractDir + File.separator 
+        		+ name + "-%s.%s";
+
+        // prepare parsing objects
+        
+        for (int i = 0; i < document.getNumberOfPages(); i++) {
+        	BufferedImage bim = renderer.renderImage(i);
+        	ImageIO.write(bim, "png", new File(String.format(out, i+1, "png")));
+        	
+        	System.out.println("DEBUG -- "+getClass().getName()+ " on page "+ (i+1));
+        }
+        document.close();
+    	
+        return dir.getAbsolutePath();
+    }
+    
+    /**
+     * Main method.
+     * @param    args    no arguments needed
+     * @throws DocumentException 
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException, DocumentException {
+
+     	String outDir = System.getProperty("java.io.tmpdir") + "pdftest" + File.separator;
+    	String test = outDir + "ND3370_Wurster_Tafelwerk_0004_Scans.pdf";
+    	
+    	System.out.println("Writing to " + outDir);
+    	new PageImageWriter().extractImages(test, outDir);
+    }
+}
