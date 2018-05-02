0fae53855a39178bbc8899b65263ccc808b4ec1d
Eva Lang <eva.lang@bistum-passau.de>
2018-05-02 09:43:34 +0200

    Adding test method to extract multiple pdfs

--- src/test/java/org/dea/util/pdf/ExtractImagesTest.java
+++ src/test/java/org/dea/util/pdf/ExtractImagesTest.java
@@ -1,6 +1,7 @@
 package org.dea.util.pdf;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
@@ -11,7 +12,34 @@ import org.slf4j.LoggerFactory;
 public class ExtractImagesTest {
 	private static final Logger logger = LoggerFactory.getLogger(ExtractImagesTest.class);
 	
-	public void extractImagesTest(String pdfPath, String tmpDirPath) throws SecurityException, IOException{
+    public File[] finder( String dirName){
+        File dir = new File(dirName);
+
+        return dir.listFiles(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				// TODO Auto-generated method stub
+				return name.endsWith(".pdf");
+			}
+		});
+
+    }
+	
+	public void extractAllPdfs(String basePath) {
+		File[] files = finder(basePath);
+		
+		for (File file : files) {
+			try {
+				logger.info("Found "+file.getAbsolutePath());
+				extractImagesTest(file.getAbsolutePath(), System.getProperty("java.io.tmpdir"), true);
+			} catch (SecurityException | IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	public void extractImagesTest(String pdfPath, String tmpDirPath, boolean keep) throws SecurityException, IOException{
 
 		final File largePdf = new File(pdfPath);
 		File tmpDir = new File(tmpDirPath);
@@ -23,8 +51,10 @@ public class ExtractImagesTest {
 		try {
 			imgWriter.extractImages(largePdf.getAbsolutePath(), tmpDir.getAbsolutePath());
 		} finally {
-			String finalOutPath = imgWriter.getExtractDirectory();
-			FileUtils.deleteDirectory(new File(finalOutPath));
+			if (!keep) {
+				String finalOutPath = imgWriter.getExtractDirectory();
+				FileUtils.deleteDirectory(new File(finalOutPath));
+			}
 		}
 		
 	}
@@ -32,19 +62,21 @@ public class ExtractImagesTest {
 //	@Test
 	public void extractImagesTest() throws SecurityException, IOException {
 		
-		extractImagesTest("/mnt/dea_scratch/TRP/bugs/large_pdf_oom/Beinecke_DL_Voynich Manuscript.pdf","/tmp/pdf/");
+		extractImagesTest("/mnt/dea_scratch/TRP/bugs/large_pdf_oom/Beinecke_DL_Voynich Manuscript.pdf","/tmp/pdf/", false);
 	}
 	
 	public static void main(String [] args) {
 		ExtractImagesTest test = new ExtractImagesTest();
 		try {
 			if (args.length > 0) {
-				test.extractImagesTest(args[0], System.getProperty("java.io.tmpdir"));
+				test.extractImagesTest(args[0], System.getProperty("java.io.tmpdir"), false);
 			} else test.extractImagesTest();
 
 		} catch (SecurityException | IOException e) {
 				// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+//		test.extractAllPdfs("C:\\Projekte\\Daten_Transkribus_Table_Collection\\datasets\\BELGRADE\\tabele\\vojna geografija");
 	}
 }
